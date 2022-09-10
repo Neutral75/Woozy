cc3bc716ead9273efa24248c7bb1b6732c1e9dbc
Cresent <98427299+Neutral75@users.noreply.github.com>
2022-09-10 12:04:50 +0100

    ☂️ Added the form

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const nano = require('./Schemas/nano.js');
 
 mongoose.connect('', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -14,5 +14,10 @@ app.use(express.static(path.join(__dirname, '../')));
 
 app.set('views', path.join(__dirname, './Website'));
 app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+    res.render('index');
+});
+
 
 app.listen(3000);
