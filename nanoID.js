895f1a365cfe11deeb795d98c135e5db041c1bad
Cresent <98427299+Neutral75@users.noreply.github.com>
2022-09-11 10:42:20 +0100

    🦄 Added functionality to the form

---
+++ nanoID.js
@@ -0,0 +1,13 @@
+const characters = 'bDfHjKlNpRtVxZAcEgIkMoQsUwY_0123456789-BdFhJkLnPrTvXzaCeGiKmOqSuW';
+
+function nanoID(length) {
+    let result = 0;
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    };
+
+    return result;
+};
+
+module.exports = nanoID;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const nano = require('./Schemas/nano.js');
+const nanoID = require('./nanoID.js');
 
 mongoose.connect('', {
     useNewUrlParser: true,
@@ -15,9 +16,16 @@ app.use(express.static(path.join(__dirname, '../')));
 app.set('views', path.join(__dirname, './Website'));
 app.set('view engine', 'ejs');
 
-app.get('/', async (req, res) => {
-    res.render('index');
+app.get('/', async (request, response) => {
+    response.render('index');
 });
 
+app.post('/nanolink', async (request, response) => {
+    await nano.create({ 
+        link: request.body.link,
+        nanolink: nanoID(7)
+    });
+    res.redirect('/');
+});
 
 app.listen(3000);
