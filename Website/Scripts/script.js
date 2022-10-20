6c8eba04b5677e5fe7f15c52cb8f0f931f3557d7
Cresent <98427299+Neutral75@users.noreply.github.com>
2022-10-20 18:38:35 +0100

    🎃 Removed halloween mode

--- Website/Scripts/script.js
+++ Website/Scripts/script.js
@@ -1,14 +1,3 @@
-function halloweenMode() {
-    const checkbox = document.getElementById('checkbox');
-    if (checkbox.checked == true) {
-        document.documentElement.style.cssText = '--primary: #EB6123';
-        document.getElementById('main-logo').src = '../Assets/woozy-orange.png';
-    } else {
-        document.documentElement.style.cssText = '--primary: #45AC5B';
-        document.getElementById('main-logo').src = '../Assets/woozy.png';
-    };
-};
-
 const forms = document.querySelector('.link');
 const links = document.querySelectorAll('.alternative-link');
 
